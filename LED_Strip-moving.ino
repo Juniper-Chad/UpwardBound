b717d63ef7ba9798482039d3b7ebf25e5091038e
Juniper-Chad <70075824+Juniper-Chad@users.noreply.github.com>
2021-09-25 01:02:46 -0500

    Update LED_Strip-moving.ino

--- LED_Strip-moving.ino
+++ LED_Strip-moving.ino
@@ -2,46 +2,43 @@
    Juniper Robotics - Grow Tower Controller LEDs
 */
 
-#include "FastLED.h"
+#include "FastLED.h"                               //include libraries for extra functionality
 #include <string.h>
 
 //define constants
-#define NUM_LEDS_PER_STRIP 10  //number of LEDs on grow light
+#define NUM_LEDS_PER_STRIP 10                      //number of LEDs on grow light
 
-CRGB ringLEDS[NUM_LEDS_PER_STRIP];  //configure LED strip with number of LEDs
+CRGB ringLEDS[NUM_LEDS_PER_STRIP];                 //configure LED strip with number of LEDs
 
 //define varaibles
-unsigned long ledMillis = 0, currentMillis = 0;
-unsigned int ledTime = 1000, index = 0, i, ratio=3;
+unsigned long lastTime = 0, currentTime = 0;
+unsigned int ledTime = 1000, index = 0, i, ratio = 3;
 
+//setup function to configure LEDs
 void setup() {
   FastLED.addLeds<NEOPIXEL, 2>(ringLEDS, NUM_LEDS_PER_STRIP);
-  FastLED.setBrightness(20);   //brightness ranges from 0-255
-  FastLED.clear();
-  FastLED.show();
+  FastLED.setBrightness(20);                       //brightness ranges from 0-255
+  FastLED.clear();                                 //turn all LEDs to off
+  FastLED.show();                                  //update LED values (actually turns off now)
 }
 
 void loop () {
-  currentMillis = millis();
-  if (currentMillis - ledMillis >= ledTime) {
-    ledMillis = currentMillis;
-    updateLEDs();
-  }
-}
-
-void updateLEDs() {
-  for (i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    if ((i + index) % ratio == 0) {
-      ringLEDS[i] = CRGB::Red; //color 1
+  currentTime = millis();                          //store current millis timer value into currentTime variable
+  if (currentTime - lastTime >= ledTime) {
+    lastTime = currentTime;                        //store current time as last time ran
+    for (i = 0; i < NUM_LEDS_PER_STRIP; i++) {     //select each individual LED on strip
+      if ((i + index) % ratio == 0) {              //this makes the color move down the strip
+        ringLEDS[i] = CRGB::Red; //color 1         //set LED color
+      }
+      else {
+        ringLEDS[i] = CRGB::Blue; //color 2        //set LED color
+      }
+      FastLED.show();                              //update LED value
     }
-    else {
-      ringLEDS[i] = CRGB::Blue; //color 2
+    index++;
+    if (index >= NUM_LEDS_PER_STRIP)  //if the position moved past the last LED on the strip
+    {
+      index = 0;                      //reset index position to the beginning
     }
-    FastLED.show();
-  }
-  index++;
-  if (index >= NUM_LEDS_PER_STRIP)  //if the index position is larger than the number of LEDs on the strip
-  {
-    index = 0;  //reset index position
   }
 }
