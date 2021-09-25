de92f2855b801f5374ce9067b704c0cc210d154e
Juniper-Chad <70075824+Juniper-Chad@users.noreply.github.com>
2021-09-25 00:47:30 -0500

    Update Upward_Bound_Pump.ino

--- Upward_Bound_Pump.ino
+++ Upward_Bound_Pump.ino
@@ -1,30 +1,30 @@
 /*
-   Juniper Robotics - Grow Tower Controller
+   Juniper Robotics - Grow Tower Controller 
 */
 
 //define constants
-#define pump_on_time 1  //on time in seconds
-#define pump_off_time 3 //off time in seconds
+#define pump_on_time 1                       //on time in seconds
+#define pump_off_time 3                      //off time in seconds
 
 //define varaibles
-long currentMillis = 0, secondsMillis = 0, secondsCount = 0;
+long currentTime = 0, lastTime = 0, secondsCount = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  //configure LED on circuit board
+  pinMode(LED_BUILTIN, OUTPUT);              //configure LED on circuit board
 }
 
 void loop () {
-  currentMillis = millis();
-  if (currentMillis - secondsMillis >= 1000) {
-    secondsMillis = currentMillis;
-    if (secondsCount >= pump_off_time) {
-      secondsCount = 0; //reset minute count to restart pump
+  currentTime = millis();                    //store current millis timer value into currentTime variable
+  if (currentTime - lastTime >= 1000) {      //check if it has been 1000ms or more since the pump ran last
+    lastTime = currentTime;                  //store current time as last time ran
+    if (secondsCount >= pump_off_time) {     //check if pump has been off for the required amount of time (pump_off_time)
+      secondsCount = 0;                      //reset minute count to restart pump
     }
-    if (secondsCount < pump_on_time) {
-      digitalWrite(LED_BUILTIN, HIGH);  //turn pump on
+    if (secondsCount < pump_on_time) {       //check if pump has been on less than the desired time (pump_on_time)
+      digitalWrite(LED_BUILTIN, HIGH);       //turn pump on
     }
-    else {
-      digitalWrite(LED_BUILTIN, LOW);  //turn pump off
+    else {  
+      digitalWrite(LED_BUILTIN, LOW);        //turn pump off
     }
     secondsCount++;
   }
